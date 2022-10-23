1a2ae5c4841f76242154f2bd0a92ed85cc738122
Mariana Stocco <mariana.stocco@outlook.com>
2022-10-23 11:10:31 -0300

    [CHANGE EVERYTHING]: devBranch

--- portfolio-mariano/src/App.js
+++ portfolio-mariano/src/App.js
@@ -1,19 +1,33 @@
-import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Landing from "./components/Landing/Landing";
-import NavBar from "./components/NavBar/NavBar";
+import React from 'react';
 
+// import components
+import Hero from './components/Hero';
+import Header from './components/Header';
+import About from './components/About';
+import GallerySection from './components/GallerySection';
+import Skills from './components/Skills';
+import Interview from './components/Interview';
+import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Copyright from './components/Copyright';
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <Landing />
-      <Home />
-      <NavBar />
-      <Footer/>
+    <div className='max-w-[1920px] mx-auto overflow-hidden bg-white'>
+      <Header />
+      <Hero />
+      <About />
+      <GallerySection />
+      <Skills />
+      <Testimonial />
+      <Interview />
+      <Contact />
+      <Footer />
+      <Copyright />
+      <div className='h-[4000px]'></div>
     </div>
   );
-}
+};
 
 export default App;
