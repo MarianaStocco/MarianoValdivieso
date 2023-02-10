12cf14f16acb7c8ca28f985a9a8eea6b296bc455
Mariana Stocco <mariana.stocco@outlook.com>
2023-02-10 19:11:21 -0300

    10/02-2

--- portfolio-mariano/src/components/Home.jsx
+++ portfolio-mariano/src/components/Home.jsx
@@ -17,7 +17,7 @@ export default function Home  ()  {
     <Hero />
     {/* <About /> */}
     <GallerySection />
-    <Interview />
+    {/* <Interview /> */}
     {/* <Skills /> */}
     {/* <Contact /> */}
     <Footer />
