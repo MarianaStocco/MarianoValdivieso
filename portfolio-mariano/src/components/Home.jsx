3cd784badda99bb9a1624ef07664134a5be4469d
Mariana Stocco <mariana.stocco@outlook.com>
2023-02-11 08:43:21 -0300

    11/02

--- portfolio-mariano/src/components/Home.jsx
+++ portfolio-mariano/src/components/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Hero from './Hero';
 import Header from './Header';
-import About from './About';
 import GallerySection from './GallerySection';
-// import Skills from './Skills';
-import Interview from './Interview';
-// import Contact from './Contact';
 import Footer from './Footer';
 import Copyright from './Copyright';
 
@@ -15,11 +11,7 @@ export default function Home  ()  {
     <>
     <Header />
     <Hero />
-    {/* <About /> */}
     <GallerySection />
-    {/* <Interview /> */}
-    {/* <Skills /> */}
-    {/* <Contact /> */}
     <Footer />
     <Copyright />
     </>
