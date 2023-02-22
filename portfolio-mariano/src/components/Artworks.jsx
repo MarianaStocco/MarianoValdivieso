8ef8a09d3d19e150267076983a74ec2754a753a0
Mariana Stocco <mariana.stocco@outlook.com>
2023-02-22 20:15:55 -0300

    22/02

--- portfolio-mariano/src/components/Artworks.jsx
+++ portfolio-mariano/src/components/Artworks.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import BuyMeACofee from '../img/gallery/bmc-full-logo-no-background.png'
+import PaypalDonate from '../img/gallery/png-transparent-paypal.png'
 // import photo album & lightbox
 import PhotoAlbum from 'react-photo-album';
 import Lightbox from 'yet-another-react-lightbox';
@@ -26,17 +27,48 @@ export default function Artworks() {
   return (
     <section>
       <div className='mt-[150px] md:w-48 md:h-auto sm:w-20 sm:h-auto'>
+        <p
+          className='ml-9 font-medium'>
+          Si quieres apoyar mi trabajo, invítame un café!
+        </p>
         <a href="https://www.buymeacoffee.com/amhiank">
-          <p
-          className='ml-9 font-light'>
-            Si quieres apoyar mi trabajo, invítame un café!
-          </p>
-        <img 
-        src={BuyMeACofee} 
-        alt="buy-me-a-coffe"
-        className='w-15 h-10  md:rounded-none  mx-auto py-2'
-         />
+          {/* <p
+            className='ml-2'>
+            * invítame un café con tarjetas!
+          </p> */}
+          <img
+            src={BuyMeACofee}
+            alt="buy-me-a-coffe"
+            className='w-15 h-10  md:rounded-none  mx-auto py-2'
+          />
         </a>
+        <a href="https://www.paypal.com/donate/?hosted_button_id=KJ687YP5RMRZC">
+          {/* <p
+            className='ml-2 font-light'>
+            * invítame un café con PayPal!
+          </p> */}
+          <img
+            src={PaypalDonate}
+            alt="donate"
+            className='w-20 h-18  md:rounded-none  mx-auto py-2 ml-9'
+          />
+        </a>
+        <a 
+        href='https://cafecito.app/amhiank'
+          rel='noopener'
+          target='_blank'>
+          {/* <p
+            className='ml-2 font-light'>
+            * invítame un café con Mercado Pago (Argentina)!
+          </p> */}
+          <img
+          className='w-15 h-10  md:rounded-none  mx-auto py-2'
+            srcset='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x'
+            src='https://cdn.cafecito.app/imgs/buttons/button_1.png'
+            alt='Invitame un café en cafecito.app'
+          />
+        </a>
+
       </div>
       <h1 className='mt-[50px] ml-32 text-lg font-extrabold'>DIGITAL ART</h1>
       <div>
